9657e8372408839f38fb259abaefe26608c68e39
phayes <patrick.d.hayes@gmail.com>
2017-05-12 17:58:30 -0700

    Lots of updates

--- ubuntu-drupal/drushrc.php
+++ ubuntu-drupal/drushrc.php
@@ -1,3 +1,8 @@
 <?php
 
+# Dont ask for confirmation of any command
 $options['yes'] = TRUE;
+
+# Always set the site to /var/www/web
+$options['root'] = '/var/www/web';
+$options['uri'] = 'http://localhost';
